f309849d7b2bc8f69ed67efed860564c7b37e6ce
qianyuqiao <767918540@qq.com>
2018-04-17 16:32:15 +0800

    task

--- 4.py
+++ 4.py
@@ -1,6 +1,25 @@
-s='NXSTasasas'
-aa='\n'.join(a for a in s.splitlines() if 'NXST' not in a )
-if not aa:
-    print'is None'
-else :
-    print  'not None'
+class Connection(object):
+    pool= None
+    def __init__(self):
+        pass
+
+
+class Pool(object):
+    def __init__(self):
+        pass
+
+def get_connection_pool(connection_cls):
+    if not connection_cls.pool:
+        connection_cls.pool=Pool()
+    return connection_cls.pool
+
+def main():
+    a=get_connection_pool(Connection)
+    b=get_connection_pool(Connection)
+    if a is b:
+        print 'yes'
+    else:
+        print 'no'
+
+if __name__ == '__main__':
+    main()
--- kombu/consumer.py
+++ kombu/consumer.py
@@ -5,23 +5,25 @@ from kombu.connection import Connection
 
 
 def process_data(body,msg):
-    print msg.payload
+    print str(msg.payload)
     msg.ack()
+def main():
+    try:
+        connection=Connection('amqp://guest:767918@192.168.8.108:5672//')
+    except Exception:
+        raise
+    print 'connecting to amqp server succeed!'
 
-try:
-    connection=Connection('amqp://guest:767918@192.168.8.108:5672//')
-except Exception:
-    raise
+    channel=connection.channel()
 
-channel=connection.channel()
+    _exchange = Exchange('media',type='direct',channel=channel)
+    video_queue = Queue('video',exchange=_exchange,routing_key='video',channel=channel)
+    consumer=Consumer(channel,queues=[video_queue],callbacks=[process_data])
+    consumer.consume()
 
-_exchange = Exchange('neutron',type='topic',channel=channel,durable=False,auto_delete=True,exclusive=False)
-video_queue = Queue('',exchange=_exchange,routing_key='q-plugin',channel=channel)
-consumer=Consumer(channel,queues=[video_queue],callbacks=[process_data])
-consumer.consume()
+    while True:
+        connection.drain_events(timeout=10)
 
-while True:
-    connection.drain_events(timeout=None)
-
-consumer.cancel()
-
+    consumer.cancel()
+if __name__ == '__main__':
+    main()
