34051213b7708df810f17b623a8c34bf53ae69d7
qianyuqiao <767918540@qq.com>
2018-04-18 22:19:54 +0800

    task

--- sql_test.py
+++ sql_test.py
@@ -1,12 +1,14 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,ForeignKey
 from sqlalchemy.orm  import sessionmaker
 from sqlalchemy import Column
 from sqlalchemy.types import CHAR,Integer,String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext import declarative
 from sqlalchemy import func,or_,not_
+from sqlalchemy import orm
 
-class UserBase(object):
+
+class MetaBase(object):
     
     @declarative.declared_attr
     def __tablename__(cls):
@@ -17,7 +19,7 @@ class UserBase(object):
             setattr(self,k,v)
 
 Session=None
-Base = declarative_base(cls=UserBase)
+Base = declarative_base(cls=MetaBase)
 
 def main():
     
@@ -25,25 +27,19 @@ def main():
     db='mysql+mysqldb://root:767918@localhost/init?charset=utf8'
     #engine is a connection managment
     engine=create_engine(db)
-
-    Session=sessionmaker(bind=engine)
-    session=Session()
-    select_all(session)
-
-
-def test(session):
-    print 'before modifying:'
-    objs = _query(session,User).all()
-
-    for obj in objs:
-        print 'Id: ',obj.Id,' name: ',obj.name,' age: ',obj.age
-
-    modify_age(session)
+    init_db(engine)
     
+    Session=sessionmaker(bind=engine,autocommit=True)
+    session=Session() 
+    
+    use(session)
+    test(session)
+
 
 def init_db(engine):
     
     Base.metadata.create_all(engine)
+
 
 def select_all(session):
     objs = _query(session,User).all()
@@ -56,18 +52,35 @@ def  drop_db(engine):
     Base.metadata.drop_all(engine)
 
 
+class Class(Base):
+    
+    class_id = Column(Integer,primary_key=True)
+    name=Column(String(10),nullable=False)
+    teacher =  Column(String(10),nullable=False)
+
+
 class User(Base):
 
     Id=Column(Integer,primary_key=True)
     name=Column(String(30))
     age = Column(Integer,nullable=True)
+    class_id = Column(Integer,ForeignKey('classs.class_id',ondelete='CASCADE'))
+    class_ = orm.relationship(Class,backref=orm.backref('users'))
 
 
 def use(session):
     
-    session.add(User(Id='1',name='a',age=12))
-    session.add(User(Id='2',name='b',age=13))
-    session.commit()
+    with session.begin(subtransactions=True):
+        session.add(Class(class_id=1,name='NO.1',teacher='Mr qian'))
+        session.add(User(Id=1,name='a',age=12,class_id=1))
+        session.add(User(Id=2,name='b',age=13,class_id=1))
+
+
+def test(session):
+    class_  = session.query(Class).filter_by(class_id=1).one()
+    print 'class_.users: ',class_ .users
+    user= session.query(User).filter_by(Id=1).one()
+    print 'user.class:',user.class_
 
 
 def modify_age(session):
@@ -102,7 +115,6 @@ def _get_by_name(session):
     
     except Exception:
         raise
-
 
 if __name__ == '__main__':
     main()
